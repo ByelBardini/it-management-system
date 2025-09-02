e0282c297eb8bffc21ffbcfddc3059bbc6fd07c1
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-09-02 11:15:58 -0300

    refactor: retirado express session e cookie parser do server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
 import cors from 'cors';
 import express from 'express';
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import session from "express-session";
 
 dotenv.config();
 
@@ -17,21 +15,6 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-app.use(cookieParser());
-app.use(
-  session({
-    secret: process.env.SECRET_KEY_USER,
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-      httpOnly: true,
-      sameSite: "Lax",
-      secure: false,
-      path: "/",
-    },
   })
 );
 
