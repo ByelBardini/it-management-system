a7b59bb50c23f15f552f039e207a9f962254e325
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-09-02 13:43:46 -0300

    feat: adiciona configuração para as chamadas de API, e chamdada do endpoint de login

---
+++ client/src/services/auth/authService.js
@@ -0,0 +1,34 @@
+import { api } from "../api.js";
+
+export async function logar(usuario_login, usuario_senha) {
+  try {
+    const { data } = await api.post("/login", { usuario_login, usuario_senha });
+    const { token, resposta } = data;
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("usuario_tipo", resposta.usuario_tipo);
+    localStorage.setItem("usuario_nome", resposta.usuario_nome);
+    localStorage.setItem("usuario_troca_senha", resposta.usuario_troca_senha);
+
+    return resposta;
+  } catch (err) {
+    if (err.response) {
+      const data = err.response.data;
+      const msg =
+        (typeof data === "string" ? data : data?.error || data?.message) || "";
+      if (msg) throw new Error(msg);
+
+      if (err.response.status === 400)
+        throw new Error("Preencha login e senha.");
+      if (err.response.status === 401)
+        throw new Error("Usuário ou senha incorretos.");
+      if (err.response.status === 403)
+        throw new Error("Usuário inativo ou sem permissão.");
+      if (err.response.status === 404)
+        throw new Error("Usuário não encontrado.");
+      if (err.response.status === 500)
+        throw new Error("Erro interno. Tente novamente.");
+    }
+    throw new Error("Falha de rede. Tente novamente.");
+  }
+}
